5c53da74f46dd4ef4c5d760cb0475e105157bc0c
怿航 <xiangzhong.wxz@alibaba-inc.com>
2015-11-01 22:10:03 +0800

    modify

--- src/read-dir.js
+++ src/read-dir.js
@@ -1,42 +1,42 @@
 var fs = require('fs'),
     path = require('path');
 
-module.exports = function(path, cb) {
+module.exports = function(p, cb){
     var allFiles = [];
 
-    if (typeof path === 'function') {
-        cb = path;
-        path = path.join(__dirname, 'i18n');
+    if(typeof p === 'function'){
+        cb = p;
+        p = path.join(__dirname, 'i18n');
     }
 
-    explorer(path);
+    explorer(p);
 
-    function explorer(path) {
-        fs.readdir(path, function(err, files) {
+    function explorer(p){
+        fs.readdir(p, function(err, files){
             //err 为错误 , files 文件名列表包含文件夹与文件
-            if (err) {
+            if(err){
                 console.log('read direct error:\n' + err);
                 return;
             }
 
             // 文件列表
-            files.forEach(function(file, index) {
-                var filePath = path.join(path, file);
+            files.forEach(function(file, index){
+                var filePath = path.join(p, file);
 
-                fs.stat(filePath, function(err, stat) {
-                    if (err) {
-                        console.log('Read file path "' + filePath + '" error:', err);
+                fs.stat(filePath, function(err, stat){
+                    if(err){
+                        console.log('Read file path "'+filePath+'" error:', err);
                         return;
                     }
-                    if (stat.isDirectory()) {
+                    if(stat.isDirectory()){
                         // 如果是文件夹抛出错误
                         throw new Error('i18n下不要再包含文件夹!');
                         // explorer(filePath);
-                    } else {
+                    }else {
                         // 读出所有的文件
                         allFiles[allFiles.length] = filePath;
 
-                        if (index === files.length - 1) {
+                        if(index === files.length-1){
                             typeof cb === 'function' && cb(allFiles);
                         }
                     }
