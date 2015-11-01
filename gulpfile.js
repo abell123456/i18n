7c854d8f6badbd5f7b038a87f34af5043536af10
怿航 <xiangzhong.wxz@alibaba-inc.com>
2015-11-01 17:31:35 +0800

    add gulp code check task!

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+console.log('=== add gulp task ===');
+var jslint = require('gulp-jslint'),
+	gulp = require('gulp');
+
+gulp.task('jslint', function() {
+	return gulp.src([
+		'./controllers/*.js',
+		'./models/*.js',
+		'./*.js'
+	]).pipe(jslint({
+		node: true,
+		nomen: true,
+		sloppy: true,
+		plusplus: true,
+		unparam: true,
+		stupid: true
+	}));
+});
+
+gulp.task('default', ['jslint']);
